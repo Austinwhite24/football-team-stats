7b9710dc3ae8bd66d2a7fb18f2298d0de0a86d81
Austin White <basketballking19aw@gmail.com>
2022-03-25 10:52:58 -0600

    2nd game done

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const path = require('path')
+// const express = require('express')
+// const path = require('path')
 
-const app = express()
+// const app = express()
 
-app.use(express.static(path.join(__dirname, '../')))
-
-
-
+// app.use(express.static(path.join(__dirname, '../public')))
+// // app.use(express.static(path.join(__dirname, '../build')))
 
 
 
 
-const port = process.env.PORT || 4006
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+
+
+// const port = process.env.PORT || 4006
+
+// app.listen(PORT, () => {
+//     console.log(`Listening on port ${PORT}`)
+// })
