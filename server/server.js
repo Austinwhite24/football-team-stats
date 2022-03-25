57aad43f81506454352c4ef1aaf4a1e4fdfecea4
Austin White <basketballking19aw@gmail.com>
2022-03-25 11:13:34 -0600

    deploy

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
-// const express = require('express')
-// const path = require('path')
+const express = require('express')
+const path = require('path')
 
-// const app = express()
+const app = express()
 
-// app.use(express.static(path.join(__dirname, '../public')))
-// // app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.use(express.static(path.resolve(__dirname, "../build")));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  });
+// app.use(express.static(path.join(__dirname, '../build')))
 
 
 
@@ -12,8 +18,8 @@
 
 
 
-// const port = process.env.PORT || 4006
+const PORT = process.env.PORT || 4006
 
-// app.listen(PORT, () => {
-//     console.log(`Listening on port ${PORT}`)
-// })
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
